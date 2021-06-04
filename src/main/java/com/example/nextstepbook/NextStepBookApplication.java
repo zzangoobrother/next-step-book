5fc0dc09971fda5ca918d481ad60be5ceaf8b367
zzanguchoisk <chltjsrkd@hanmail.net>
2021-06-04 10:11:18 +0900

    Initial commit

---
+++ src/main/java/com/example/nextstepbook/NextStepBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.nextstepbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextStepBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextStepBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/nextstepbook/NextStepBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nextstepbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextStepBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
